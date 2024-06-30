bc0558b1218274853d4b0b67f69704d03f9ea597
Deepak <deepaknnayak34@gmail.com>
2024-06-30 22:23:05 +0530

    update

--- Javascript/Looping/notes.js
+++ Javascript/Looping/notes.js
@@ -62,10 +62,44 @@
  * Unary Operator:
  * ----------------
  * let a = 1; 
- * console.log(a++) => this means it will first print the value of a and then increase the value of a
+ * console.log(a++) => this means it will first print the value of a which is 1 and then increase the value of a
  * console.log(a) => 2 here i will get the increment value of a. 
  * a++ => postfix (pehle use ho gaya and increment baad mein)
  * 
  * 
  * ++a => eska matlab hai prefix operator pehle increment hoga andd then ham usko use karte hein.
+ */
+
+/**
+ * Break:
+ * ------
+ * break terminates the nearest loop immediately.
+ * agger kisi bhi point of time pe aggar harama loop break ko hit hua then 
+ * us case mein loop immediately exit out kar jaega.
+ * 
+ * lets discuss it properly with an example: 
+ * -------------------------------------------
+ * let i = 0;  
+ * while(i<=10){
+ *  if(i % 4 ==0){
+ *      break;
+ *  }
+ * console.log(i)
+ * i+=1
+ * 
+ * es example mein hamara nearest loop konsa hai, line number 83 se shuru hua hai nearest loop 
+ * toh woh hame line number 83 wale loop se bahar nikal dega. 
+}
+ */
+
+/**
+ * Continue:
+ * ---------
+ * continue brings you back to the nearest loop.
+ */
+
+/**
+ * Functions:
+ * ----------
+ * 
  */

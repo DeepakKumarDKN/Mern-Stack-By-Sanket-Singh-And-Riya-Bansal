15fb8c7b38a10dfcd12bb6376d1321e6dbf06e46
Deepak <deepaknnayak34@gmail.com>
2024-07-03 13:58:02 +0530

    notes added on array

--- Javascript/Arrays/notes.js
+++ Javascript/Arrays/notes.js
@@ -26,3 +26,55 @@ console.log(arr2[10])
 
 console.log(arr2)
 
+//* Another way for creating array with new key word. 
+
+const arr3 = new Array(10,20,30,40) // this will also give us an Array as Ouput. 
+console.log(arr3)
+
+
+const arr4 = new Array(4)
+console.log(arr4) 
+// Output: 
+//---------
+//[ <4 empty items> ]
+// yahan meine ek single value diya hai array banane ke liye but in result mujhe 
+// utna seize ka array mil raha hai aur har single element mein mujhe undefined hi milega 
+// aggar mein access karunga toh 
+
+// par aggar meine multiple element use karke banaunga jaise meine line number 31 mein banaya hai 
+// toh mujhe ek propery array array with actual values without getting undefined (in sort un elements ke saath ek array milega)
+
+const array5 = new Array(5).fill('hello')
+console.log(array5)
+
+// yahan pe mujhe 5 length ki array milega aur sab mein hello dikhne ko milega
+
+//* find length in an array.
+let arr6 = [1,2,3,4,5,6]
+console.log(arr6.length)
+
+//* push element into an array that too in the last. 
+
+arr6.push(7)
+console.log(arr6)
+
+//* remove the element from an array
+
+arr6.pop()
+console.log(arr6)
+
+//* shift jo starting mein se element ko remove kar dega. 
+//* unshift jo starting mein se element ko add kar dega.
+
+arr6.shift()
+console.log(arr6)
+
+arr6.unshift(1)
+console.log(arr6)
+
+//* indexof in array 
+console.log(arr6.indexOf(100))
+// aggar hai toh proper index milega aur aggar nahi hai toh -1 milega
+
+//* reverse => we can reverse the complete array. 
+console.log(arr6.reverse())

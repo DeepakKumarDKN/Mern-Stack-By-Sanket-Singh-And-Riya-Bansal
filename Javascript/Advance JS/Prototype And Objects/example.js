6e005df27a369cdf5764d23b57b2d3451bed308c
Deepak <deepaknnayak34@gmail.com>
2024-09-15 00:04:28 +0530

    updated

--- Javascript/Advance JS/Prototype And Objects/example.js
+++ Javascript/Advance JS/Prototype And Objects/example.js
@@ -59,9 +59,33 @@
 // data.fn()
 
 
-function getValues(){
-	x=10;
-	y=20
+let obj = {
+	x:1, 
+	y:2,
+	fn: function(){
+		const printVariables = () => {
+			console.log(this.x, this.y)
+			console.log(x,y)
+		}
+		printVariables()
+
+	const printVariables1 = () => {
+		this.x=3
+		this.y=4
+		console.log(this.x, this.y)
+		console.log(x,y)
+	}
+	printVariables1()
+	}
+
 }
 
-getValues()
+obj.fn()
+
+// in the above code we need to remobe console.log(x,y) line number 68 because 
+// we havn't defined any x and y here we are just trying to access the key value pair
+// same goes with line number 76 there also wehave to remove the line 
+
+// now come to printVariables1 there the value of x inside the object will be  updated 
+// because in line number 73,74 we are trying to update the value of x in line number 
+// 63,64 and it will get updated also so we will get 3,4 as output
